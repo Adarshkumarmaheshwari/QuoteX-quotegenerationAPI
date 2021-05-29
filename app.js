aa1be25e8b6fcf8311edb8de34843cd68502dd99
Adarsh Kumar Maheshwari <76864130+Adarshkumarmaheshwari@users.noreply.github.com>
2021-05-29 13:19:10 +0530

    bug fix

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/", (req, res) => {
   res.json(req.body.post);
 });
 
-const port = 5000 || process.env.PORT;
+const port =process.env.PORT ||  5000;
 
 app.listen(port, () => {
   console.log(
